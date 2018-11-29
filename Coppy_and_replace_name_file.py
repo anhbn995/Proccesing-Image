df5bcb8a9d3d60cce12cdb949273cecfb2328156
giangblackk <giangblackk@gmail.com>
2018-11-29 16:39:40 +0700

    sua cai os.chdir

--- Coppy_and_replace_name_file.py
+++ Coppy_and_replace_name_file.py
@@ -14,36 +14,33 @@ def create_list_id(path):
 
 # tao nhieu folder# o day nhieu folder nen name_folders = [folder1, folder2, ...]
 def make_multiple_folder(root_path, name_folders):
+    os.chdir(root_path)
     for folder in name_folders:
-        if os.path.isdir(os.path.join(root_path,folder)):
+        if os.path.isdir(folder):
             # print(folder)
             continue
         else:
-            os.makedirs(os.path.join(root_path,folder))
-# root_path = "F:"
-# folder = ["lol","xoa","tao"]
-# make_multiple_folder(root_path,folder)
+            os.makedirs(folder)
 
 # di chuyển file sang thư mục theo định dạng file
 def move_file_and_rename(path_source, name_folders, str_move):
+    os.chdir(path_source)
     # tạo folder de chuyen cai da
     make_multiple_folder (path_source, name_folders)
 
     #bat dau di chuyen file va sua ten no theo dinh dang cua coco
-    for file in glob.glob(path_source + "\*.tif"):
-        if str_move in file:
-            new_name_file = file.replace("_" + str_move,"")
-            print(str_move)
-            print(os.path.join(path_source, str_move, new_name_file))
-            # copyfile(os.path.join(path_source,file), os.path.join(path_source,str_move,new_name_file))
+    for f in glob.glob("*.tif"):
+        # print(file)
+        if str_move in f:
+            new_name_file = f.replace("_" + str_move,"")
+            path_copy = os.path.join(path_source,str_move)
+            copyfile(os.path.join(path_source,f), os.path.join(path_copy,new_name_file))
         else:
             continue
 
-path_source = r"F:\Xoa\Xoatiep"
-# path_destination = "F:\Xoatiep", path_destination
+path_source = "/media/khoi/Data/ChangeDetection/Bangalore/Xonglaxoa"
 
-# move_file_and_rename(path_source, path_destination)
 name_folders = ["label","image","base"]
-move_file_and_rename(path_source, name_folders, "label")
+move_file_and_rename(path_source, name_folders, "image")
 
 
